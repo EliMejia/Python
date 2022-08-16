84d64ad20fece26a66c76ae2f27ebc6157b2d59e
EliMejia <103217622+EliMejia@users.noreply.github.com>
2022-08-16 14:33:49 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,17 @@
+#VARIABLES
+
+""" Para facilitar la programacion existen varibles que sirven para almacenar informacion que se puede
+utilizar en mas de una ocasiones, el valor de la varible puede ir cambiando conforme el desarrollo: """
+
+#EJEMPLO
+
+my_name="Eli" 
+
+print my_name
+
+
+#Puede contener guiones bajos o numeros pero no pueden ir al inicio Ejemplo:
+
+#Error: 1321_name
+
+#Correcto: name_1321
