6a914bab04959ff8131be3c4f03a180fb3122010
EliMejia <103217622+EliMejia@users.noreply.github.com>
2022-08-17 22:36:09 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -15,3 +15,28 @@ print my_name
 #Error: 1321_name
 
 #Correcto: name_1321
+
+"""
+En programacion existen dos tipos de variables:
+
+1.- GLOBALES: su valor se puede utilizar en todo el progtrama ademas de qie es posible cambiar su
+valor en el transcurso de este.
+
+2.- LOCALES: su valor solo se puede utilizar o modificar dentro de la funcion, las valiables 
+locales son las que se definen dentro de una funcion
+
+NOTA:Aunque el valor de la variable global cambie se puede utilizar en una funcion su valor
+original con..... global nombre_variable
+"""
+
+#Ejemplo
+
+num=4
+
+def fun1():
+  global num
+  num=5
+ 
+print num #El resultado sera 4 ya que todavia no se aplican los cambios
+fun1() #Se llama a la funcion que cambia el valor del numero
+print num #El resultado sera 5 ya que el cambio de valor fue hecho anteriormente en el numero
