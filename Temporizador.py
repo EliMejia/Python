71431616ea0a6ba870a520a2c1308fdf032e9fab
EliMejia <103217622+EliMejia@users.noreply.github.com>
2022-08-17 22:48:56 -0500

    Create Temporizador.py

---
+++ Temporizador.py
@@ -0,0 +1,18 @@
+#Temporizador
+
+"""
+Se puede agregar un temporizador que permita realizar una funcion cada 
+cierto tiempo, se debe tener en cuenta que se maneja en milisegundos
+"""
+
+#Ejemplo:
+
+import simplegui
+
+def tick()
+  name="ELI"
+  print name+"MEJIA"
+                            #(Cada cuando se ejecuta, nombre de la funcion que ejecuta)
+ timer=simplegui.create_timer(100,tick)
+ time.start() #Da inicio al temporizador
+ time.stop()  #Detiene el temporizador
