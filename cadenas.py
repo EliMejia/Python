dddd862e8edd761350a0410705835a2a241ff96f
EliMejia <103217622+EliMejia@users.noreply.github.com>
2022-08-21 20:03:11 -0500

    Create cadenas.py

---
+++ cadenas.py
@@ -0,0 +1,37 @@
+#CADENAS
+
+"""
+Con las cadenas se pueden utilizar diferentes metodos para
+obtner una letra, un rango o una parte especifica de la cadena
+"""
+
+#si dentro de la cade se quieren poner comillas simples o dobles se usan las contrarias 
+
+s1="'Hola' Ejemplo1" 
+s2='"Hola" Ejemplo2'
+
+#Juntar cadenas y variables 
+
+name="Eli"
+s3='Mi'+'Nombre'+'es'+name
+
+#Imprimir una letra de la cadena
+
+print s1[3]
+
+#Saber la longitud de la cadena 
+
+print len[s1]
+
+#Imprimir un rango de letras
+
+print s1[0:2]
+
+#Imprimir hasta cierto numero de caracteres
+
+print s2[:6]
+
+#Imprimir el resto de caracteres 
+
+print s2[10:]
+
